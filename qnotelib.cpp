14700144b9bf22bce8ea01fdfd0dd49baeb8b23a
Paul Bae <pbae@utexas.edu>
2014-09-10 01:25:54 -0500

    Created a new helper class

---
+++ qnotelib.cpp
@@ -0,0 +1,5 @@
+#include "qnotelib.h"
+
+QNoteLib::QNoteLib()
+{
+}
---
+++ qnotelib.h
@@ -0,0 +1,10 @@
+#ifndef QNOTELIB_H
+#define QNOTELIB_H
+
+class QNoteLib
+{
+public:
+    QNoteLib();
+};
+
+#endif // QNOTELIB_H
